f801508a6bae80a8a7159b1099fa213c358f7305
Will Duquette <William.H.Duquette@jpl.nasa.gov>
2008-10-31 20:35:04 +0000

    Added the minlib(n) package, and its first module, mapref(n).

---
+++ lib/minlib/minlib.tcl
@@ -0,0 +1,52 @@
+#-----------------------------------------------------------------------
+# TITLE:
+#    minlib.tcl
+#
+# AUTHOR:
+#    Will Duquette
+#
+# DESCRIPTION:
+#    Minerva: minlib(n) main package
+#
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# External Package Dependencies
+
+package require snit
+
+#-----------------------------------------------------------------------
+# Internal Package Dependencies
+
+package require marsutil
+package require sqlite3
+
+#-----------------------------------------------------------------------
+# Package Definition
+
+package provide minlib 1.0
+
+#-----------------------------------------------------------------------
+# Namespace definition
+
+namespace eval ::minlib:: {
+    variable library [file dirname [info script]]
+
+    namespace import ::marsutil::* 
+}
+
+#-------------------------------------------------------------------
+# Load binary extensions, if present.
+
+set binlib [file join $::minlib::library libVersion.so]
+
+if {[file exists $binlib]} {
+    load $binlib
+}
+
+#-----------------------------------------------------------------------
+# Load minlib(n) submodules
+
+source [file join $::minlib::library mapref.tcl]
+
+
---
+++ lib/minlib/pkgIndex.tcl
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------
+# TITLE:
+#    pkgIndex.tcl
+#
+# AUTHOR:
+#    William H. Duquette
+#
+# DESCRIPTION:
+#    Minerva: minlib(n) package index file
+
+package ifneeded minlib 1.0 [list source [file join $dir minlib.tcl]]
+
+
