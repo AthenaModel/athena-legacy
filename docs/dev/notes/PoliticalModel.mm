3587c904e94e602659207cc7a3ebb1fc04991ab2
Will Duquette <William.H.Duquette@jpl.nasa.gov>
2009-06-10 22:23:23 +0000

    Updated politics model mindmap

--- docs/dev/notes/PoliticalModel.mm
+++ docs/dev/notes/PoliticalModel.mm
@@ -1,45 +1,52 @@
 <map version="0.8.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1244493211931" ID="Freemind_Link_1983795013" MODIFIED="1244493219662" TEXT="Political Model">
+<node CREATED="1244493211931" ID="Freemind_Link_1983795013" MODIFIED="1244662996889" TEXT="Politics Model">
 <node CREATED="1244493326882" ID="_" MODIFIED="1244493330063" POSITION="right" TEXT="Actors">
 <node CREATED="1244493330810" ID="Freemind_Link_923763143" MODIFIED="1244493338010" TEXT="Attributes">
+<node CREATED="1244493502731" ID="Freemind_Link_1659467862" MODIFIED="1244493503855" TEXT="Goals">
+<node CREATED="1244493468772" ID="Freemind_Link_1685992509" MODIFIED="1244493476191" TEXT="Positions on Fearey Charts"/>
+<node CREATED="1244493948196" ID="Freemind_Link_1937123078" MODIFIED="1244493953303" TEXT="Narrative description of goals">
+<node CREATED="1244493954171" ID="Freemind_Link_1369872047" MODIFIED="1244493962655" TEXT="This is not represented in the software">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+</node>
+<node CREATED="1244493595667" FOLDED="true" ID="Freemind_Link_1945472764" MODIFIED="1244493597536" TEXT="Assets">
+<node CREATED="1244493597851" ID="Freemind_Link_1384103635" MODIFIED="1244493603047" TEXT="Used to compute clout"/>
+<node CREATED="1244493603371" ID="Freemind_Link_1227147455" MODIFIED="1244493608326" TEXT="Also used for other purposes"/>
+</node>
 <node CREATED="1244493338850" ID="Freemind_Link_1734483482" MODIFIED="1244493350647" TEXT="Clout">
-<node CREATED="1244493351194" ID="Freemind_Link_145761170" MODIFIED="1244493354654" TEXT="Composite">
-<node CREATED="1244493775158" ID="Freemind_Link_1047389110" MODIFIED="1244497728024" TEXT="How computed?">
-<node CREATED="1244497086181" ID="Freemind_Link_1505723664" MODIFIED="1244497092480" TEXT="Weighted sum of individual clouts"/>
-<node CREATED="1244497241654" ID="Freemind_Link_151121445" MODIFIED="1244497246322" TEXT="Weights chosen by the user"/>
-<node CREATED="1244497536945" ID="Freemind_Link_688266078" MODIFIED="1244497543028" TEXT="Scenario data, not model parameters"/>
-</node>
-</node>
 <node CREATED="1244493355122" ID="Freemind_Link_1313579747" MODIFIED="1244496145562" TEXT="Political">
 <node CREATED="1244496799624" ID="Freemind_Link_42645355" MODIFIED="1244496810557" TEXT="One&apos;s control over the political process">
 <node CREATED="1244496810872" ID="Freemind_Link_283600638" MODIFIED="1244496827595" TEXT="E.g., seats in Parliament"/>
 </node>
+<node CREATED="1244493895340" ID="Freemind_Link_170616889" MODIFIED="1244494926449" TEXT="What assets?">
+<node CREATED="1244494906417" ID="Freemind_Link_1365626590" MODIFIED="1244663745766" TEXT="No modeled assets in Spiral 1"/>
+</node>
 <node CREATED="1244493783662" ID="Freemind_Link_310941994" MODIFIED="1244494926444" TEXT="How computed?">
 <node CREATED="1244494827185" ID="Freemind_Link_1066979579" MODIFIED="1244495760340" TEXT="A number picked by the analyst"/>
 </node>
-<node CREATED="1244493895340" ID="Freemind_Link_170616889" MODIFIED="1244494926449" TEXT="What assets?">
-<node CREATED="1244494906417" ID="Freemind_Link_1365626590" MODIFIED="1244494916844" TEXT="No specific assets in Spiral 1"/>
-</node>
 </node>
 <node CREATED="1244493378578" ID="Freemind_Link_237596859" MODIFIED="1244493403453" TEXT="Information">
+<node CREATED="1244663752592" ID="Freemind_Link_1937757880" MODIFIED="1244663874075" TEXT="Control of media outlets, etc."/>
+<node CREATED="1244493895340" ID="Freemind_Link_550368414" MODIFIED="1244495752655" TEXT="What assets?">
+<node CREATED="1244494906417" ID="Freemind_Link_1428288105" MODIFIED="1244663776438" TEXT="No modeled assets in Spiral 1"/>
+</node>
 <node CREATED="1244493787686" ID="Freemind_Link_141100175" MODIFIED="1244495769006" TEXT="How computed?">
 <node CREATED="1244494827185" ID="Freemind_Link_1287860591" MODIFIED="1244495760340" TEXT="A number picked by the analyst"/>
 </node>
-<node CREATED="1244493895340" ID="Freemind_Link_550368414" MODIFIED="1244495752655" TEXT="What assets?">
-<node CREATED="1244494906417" ID="Freemind_Link_1428288105" MODIFIED="1244494916844" TEXT="No specific assets in Spiral 1"/>
-</node>
 </node>
 <node CREATED="1244493372210" ID="Freemind_Link_697510736" MODIFIED="1244493374014" TEXT="Military">
-<node CREATED="1244493791046" ID="Freemind_Link_513608637" MODIFIED="1244496573549" TEXT="How computed?">
-<node CREATED="1244496361363" ID="Freemind_Link_1457526435" MODIFIED="1244496372030" TEXT="Add up for positive relationships, across groups and neighborhoods"/>
-</node>
 <node CREATED="1244493895340" ID="Freemind_Link_1808503795" MODIFIED="1244496577516" TEXT="What assets?">
-<node CREATED="1244495930925" ID="Freemind_Link_1973019747" MODIFIED="1244496294149" TEXT="Relationship with groups">
-<node CREATED="1244495951037" ID="Freemind_Link_1646668526" MODIFIED="1244495996975" TEXT="-1.0 to 1.0"/>
+<node CREATED="1244665024089" ID="Freemind_Link_1552229547" MODIFIED="1244666131985" TEXT="Force groups can have relationships with actors."/>
+<node CREATED="1244666133206" ID="Freemind_Link_114479784" MODIFIED="1244666269289" TEXT="A positive relationship (0.0 to 1.0) indicates the &#xa;extent to which the group will take orders from the actor"/>
+<node CREATED="1244666191581" ID="Freemind_Link_441503953" MODIFIED="1244666209392" TEXT="Negative relationships are possible, but it&apos;s not clear how we&apos;d use them."/>
+<node CREATED="1244665294279" ID="Freemind_Link_1903064784" MODIFIED="1244666294727" TEXT="BLUE&apos;s relationship with the United States is 1.0"/>
+<node CREATED="1244496010477" ID="Freemind_Link_790907216" MODIFIED="1244664381183" TEXT="Only positive relationships contribute to clout"/>
 </node>
-<node CREATED="1244496295878" ID="Freemind_Link_351840063" MODIFIED="1244496304786" TEXT="Force of each group, as computed already"/>
-<node CREATED="1244496010477" ID="Freemind_Link_790907216" MODIFIED="1244496021401" TEXT="Positive relationships contribute to clout"/>
+<node CREATED="1244493791046" ID="Freemind_Link_513608637" MODIFIED="1244496573549" TEXT="How computed?">
+<node CREATED="1244496295878" ID="Freemind_Link_351840063" MODIFIED="1244666234656" TEXT="&quot;Own&quot; Force of each force group in each neighborhood, as computed already by the security model"/>
+<node CREATED="1244496361363" ID="Freemind_Link_1457526435" MODIFIED="1244664369607" TEXT="Add up force weighted by positive relationships, across groups and neighborhoods"/>
 </node>
 </node>
 <node CREATED="1244493369114" ID="Freemind_Link_221353925" MODIFIED="1244493371630" TEXT="Economic">
@@ -52,25 +59,23 @@
 </node>
 <node CREATED="1244493375570" ID="Freemind_Link_1582733280" MODIFIED="1244493377757" TEXT="Cultural">
 <node CREATED="1244493895340" ID="Freemind_Link_1576649565" MODIFIED="1244496843627" TEXT="What assets?">
-<node CREATED="1244494906417" ID="Freemind_Link_721820048" MODIFIED="1244496481622" TEXT="Positive relationships with neighborhood groups"/>
+<node CREATED="1244666527019" ID="Freemind_Link_1865852953" MODIFIED="1244666543902" TEXT="Neighborhood groups can have relationships with actors.">
+<node CREATED="1244666544666" ID="Freemind_Link_631050433" MODIFIED="1244666565662" TEXT="E.g., tribal leaders"/>
+</node>
+<node CREATED="1244494906417" ID="Freemind_Link_721820048" MODIFIED="1244666858345" TEXT="A positive relationship indicates the extent to which&#xa;the neighborhood group will support the actor based&#xa;on who he is in the culture."/>
 <node CREATED="1244496460579" ID="Freemind_Link_1559099431" MODIFIED="1244496466710" TEXT="Population of these groups"/>
 </node>
 <node CREATED="1244493799094" ID="Freemind_Link_844103884" MODIFIED="1244496848683" TEXT="How computed?">
-<node CREATED="1244494827185" ID="Freemind_Link_579966146" MODIFIED="1244496542050" TEXT="Total population weighted by relationships?"/>
+<node CREATED="1244494827185" ID="Freemind_Link_579966146" MODIFIED="1244666868232" TEXT="Total population weighted by relationships"/>
 </node>
 </node>
-</node>
-<node CREATED="1244493502731" ID="Freemind_Link_1659467862" MODIFIED="1244493503855" TEXT="Goals">
-<node CREATED="1244493468772" ID="Freemind_Link_1685992509" MODIFIED="1244493476191" TEXT="Positions on Fearey Charts"/>
-<node CREATED="1244493948196" ID="Freemind_Link_1937123078" MODIFIED="1244493953303" TEXT="Narrative description of goals">
-<node CREATED="1244493954171" ID="Freemind_Link_1369872047" MODIFIED="1244493962655" TEXT="This is not represented in the software">
-<icon BUILTIN="messagebox_warning"/>
-</node>
+<node CREATED="1244493351194" ID="Freemind_Link_145761170" MODIFIED="1244493354654" TEXT="Composite">
+<node CREATED="1244493775158" ID="Freemind_Link_1047389110" MODIFIED="1244497728024" TEXT="How computed?">
+<node CREATED="1244497086181" ID="Freemind_Link_1505723664" MODIFIED="1244497092480" TEXT="Weighted sum of individual clouts"/>
+<node CREATED="1244497241654" ID="Freemind_Link_151121445" MODIFIED="1244497246322" TEXT="Weights chosen by the user"/>
+<node CREATED="1244497536945" ID="Freemind_Link_688266078" MODIFIED="1244497543028" TEXT="Scenario data, not model parameters"/>
 </node>
 </node>
-<node CREATED="1244493595667" ID="Freemind_Link_1945472764" MODIFIED="1244493597536" TEXT="Assets">
-<node CREATED="1244493597851" ID="Freemind_Link_1384103635" MODIFIED="1244493603047" TEXT="Used to compute clout"/>
-<node CREATED="1244493603371" ID="Freemind_Link_1227147455" MODIFIED="1244493608326" TEXT="Also used for other purposes"/>
 </node>
 </node>
 </node>
@@ -79,7 +84,8 @@
 <node CREATED="1244493567019" ID="Freemind_Link_162336324" MODIFIED="1244493571663" TEXT="Current state"/>
 <node CREATED="1244493615907" ID="Freemind_Link_633176724" MODIFIED="1244493884936" TEXT="Effect of current state">
 <icon BUILTIN="help"/>
-<node CREATED="1244498032717" ID="Freemind_Link_1146420909" MODIFIED="1244498038161" TEXT="Change in government budget">
+<node CREATED="1244667033240" ID="Freemind_Link_955856007" MODIFIED="1244667039452" TEXT="Report to user as state changes"/>
+<node CREATED="1244498032717" ID="Freemind_Link_1146420909" MODIFIED="1244667106619" TEXT="Change in government budget and tax rates">
 <node CREATED="1244498047469" ID="Freemind_Link_1797558404" MODIFIED="1244498052777" TEXT="But no government in Spiral 1"/>
 </node>
 </node>
@@ -88,35 +94,31 @@
 <node CREATED="1244494591863" ID="Freemind_Link_649214120" MODIFIED="1244494679959" TEXT="Composite can be computed according &#xa;to formula chosen by analyst">
 <node CREATED="1244494637903" ID="Freemind_Link_1044224653" MODIFIED="1244494642066" TEXT="Clout-weighted average"/>
 <node CREATED="1244494642359" ID="Freemind_Link_690793408" MODIFIED="1244494651778" TEXT="Winner takes all"/>
-<node CREATED="1244494652183" ID="Freemind_Link_461321475" MODIFIED="1244494667066" TEXT="Chosen by chart"/>
+<node CREATED="1244494652183" ID="Freemind_Link_461321475" MODIFIED="1244667125115" TEXT="Can be different for different charts"/>
 </node>
 <node CREATED="1244494150102" ID="Freemind_Link_873883329" MODIFIED="1244494588371" TEXT="Current state can be set by the analyst"/>
 <node CREATED="1244494701806" ID="Freemind_Link_1776671338" MODIFIED="1244494726033" TEXT="Current state drifts toward composite over time&#xa;at a percentage rate."/>
 </node>
 <node CREATED="1244495306033" ID="Freemind_Link_975038208" MODIFIED="1244495315462" TEXT="Chart Types">
-<node CREATED="1244495316265" ID="Freemind_Link_1386628765" MODIFIED="1244497835465" TEXT="Triangular">
-<node CREATED="1244499259233" ID="Freemind_Link_957395708" MODIFIED="1244499282356" TEXT="Initial system is an input"/>
-<node CREATED="1244498570867" ID="Freemind_Link_1248466457" MODIFIED="1244498583423" TEXT="Corners indicate government type">
+<node CREATED="1244495316265" ID="Freemind_Link_1386628765" MODIFIED="1244667271593" TEXT="Government Type">
+<node CREATED="1244667153079" ID="Freemind_Link_1901830943" MODIFIED="1244667168610" TEXT="Controls goverment type"/>
+<node CREATED="1244667274893" ID="Freemind_Link_1260990438" MODIFIED="1244667279408" TEXT="Chart is triangular">
 <node CREATED="1244498583738" ID="Freemind_Link_1513830044" MODIFIED="1244498591693" TEXT="Military dictatorship"/>
 <node CREATED="1244498592114" ID="Freemind_Link_1709002883" MODIFIED="1244498596221" TEXT="Theocracy"/>
 <node CREATED="1244498596522" ID="Freemind_Link_1512170697" MODIFIED="1244498608933" TEXT="Secular democracy"/>
 </node>
-<node CREATED="1244498907310" ID="Freemind_Link_1643514856" MODIFIED="1244498939870" TEXT="Moving into a corner makes the &#xa;system change into that system.">
+<node CREATED="1244499259233" ID="Freemind_Link_957395708" MODIFIED="1244667226529" TEXT="Initial government type is an input"/>
+<node CREATED="1244498907310" ID="Freemind_Link_1643514856" MODIFIED="1244667245714" TEXT="Moving into a corner changes the government type&#xa;">
 <node CREATED="1244499203617" ID="Freemind_Link_1962782816" MODIFIED="1244499207916" TEXT="Notification to the user"/>
-<node CREATED="1244499210738" ID="Freemind_Link_388648619" MODIFIED="1244499234532" TEXT="Maybe other effects of current system."/>
-<node CREATED="1244499289105" ID="Freemind_Link_724021562" MODIFIED="1244499298076" TEXT="Change from one system to another could affect attitudes"/>
+<node CREATED="1244499210738" ID="Freemind_Link_388648619" MODIFIED="1244667259393" TEXT="Maybe other effects of current government type."/>
+<node CREATED="1244499289105" ID="Freemind_Link_724021562" MODIFIED="1244667325312" TEXT="Change from one type to another could affect attitudes"/>
 </node>
 <node CREATED="1244500286392" ID="Freemind_Link_477139068" MODIFIED="1244500291813" TEXT="How to represent a position?">
-<node CREATED="1244500292160" ID="Freemind_Link_1061590448" MODIFIED="1244500595483" TEXT="Weights on the three corners?"/>
-<node CREATED="1244500595903" ID="Freemind_Link_1664017851" MODIFIED="1244500599179" TEXT="Distance from center?"/>
-<node CREATED="1244500599623" ID="Freemind_Link_378639103" MODIFIED="1244501802076" TEXT="X,Y"/>
-<node CREATED="1244559030691" ID="Freemind_Link_1023474616" MODIFIED="1244559037230" TEXT="Closeness to the three corners?"/>
+<node CREATED="1244559030691" ID="Freemind_Link_1023474616" MODIFIED="1244667344199" TEXT="Closeness to the three corners"/>
+<node CREATED="1244500599623" ID="Freemind_Link_378639103" MODIFIED="1244667362471" TEXT="X,Y (internally)"/>
 </node>
 </node>
-<node CREATED="1244495318720" ID="Freemind_Link_1346249967" MODIFIED="1244497840857" TEXT="Square">
-<node CREATED="1244499389301" ID="Freemind_Link_335567056" MODIFIED="1244499393920" TEXT="Not clear what axes are"/>
 <node CREATED="1244500195643" ID="Freemind_Link_963250918" MODIFIED="1244500207222" TEXT="Fearey charts for later spirals are TBD"/>
-</node>
 </node>
 </node>
 </node>
