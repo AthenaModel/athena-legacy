7a8649e20d5b8a54240484eebdb2fed6eff5380f
Will Duquette <William.H.Duquette@jpl.nasa.gov>
2009-06-19 18:04:36 +0000

    Notes saved from SME meeting

---
+++ docs/dev/notes/SMEmeetingDecisions0906.mm
@@ -0,0 +1,84 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1245425011163" ID="Freemind_Link_1087595761" MODIFIED="1245426188522" TEXT="SME Meeting Decisions&#xa;16-18 June 2009">
+<node CREATED="1245425084796" ID="_" MODIFIED="1245425090279" POSITION="right" TEXT="Intermittent Activities">
+<node CREATED="1245434565553" ID="Freemind_Link_782860870" MODIFIED="1245434572065" TEXT="Spiral 2">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1245425380974" ID="Freemind_Link_156357358" MODIFIED="1245425398651" TEXT="Need to model intermittent activities like Interview/Screen and Cordon and Search."/>
+<node CREATED="1245425400892" ID="Freemind_Link_738846660" MODIFIED="1245425416299" TEXT="Such activities presumably draw troops from some other activity, e.g., PATROL"/>
+<node CREATED="1245425090796" ID="Freemind_Link_193760284" MODIFIED="1245425421846" TEXT="It&apos;s not clear whether doing I/S or C&amp;S should diminish the impact of the troop&apos;s main activity. "/>
+<node CREATED="1245425422556" ID="Freemind_Link_1566602261" MODIFIED="1245425448742" TEXT="Situation or series of event?"/>
+</node>
+<node CREATED="1245425469228" ID="Freemind_Link_917075298" MODIFIED="1245425471814" POSITION="right" TEXT="Shifts">
+<node CREATED="1245425472252" ID="Freemind_Link_170615432" MODIFIED="1245425491302" TEXT="The assigned-to-active ratio is simply the number of shifts per day."/>
+<node CREATED="1245425491852" ID="Freemind_Link_304605190" MODIFIED="1245425494198" TEXT="So call it that."/>
+</node>
+<node CREATED="1245425507165" ID="Freemind_Link_1211960668" MODIFIED="1245425576262" POSITION="right" TEXT="Displaced Persons/Refugees">
+<node CREATED="1245425513869" ID="Freemind_Link_736067438" MODIFIED="1245425550022" TEXT="Terminology">
+<node CREATED="1245425533516" ID="Freemind_Link_857064222" MODIFIED="1245425544742" TEXT="Displaced persons remain in their own country"/>
+<node CREATED="1245425545068" ID="Freemind_Link_152664614" MODIFIED="1245425558758" TEXT="Refugees have left their own country"/>
+</node>
+<node CREATED="1245425561500" ID="Freemind_Link_950741708" MODIFIED="1245425649366" TEXT="Effect of displaced persons/refugees is not affected by the terminology.">
+<icon BUILTIN="ksmiletris"/>
+</node>
+<node CREATED="1245425654555" ID="Freemind_Link_554326188" MODIFIED="1245426010803" TEXT="Camps are sometimes located near local civilians">
+<node CREATED="1245425680812" ID="Freemind_Link_23685306" MODIFIED="1245426127349" TEXT="But not for Spiral 1"/>
+</node>
+<node CREATED="1245425998972" ID="Freemind_Link_3649449" MODIFIED="1245426043833" TEXT="True statements?">
+<icon BUILTIN="help"/>
+<node CREATED="1245426023228" ID="Freemind_Link_807829845" MODIFIED="1245426030325" TEXT="All Refugees are Displaced Persons"/>
+<node CREATED="1245426030796" ID="Freemind_Link_688419828" MODIFIED="1245426037237" TEXT="Not all Displaced Persons are Refugees"/>
+</node>
+<node CREATED="1245425688140" ID="Freemind_Link_643317493" MODIFIED="1245426119045" TEXT="For Spiral 1">
+<node CREATED="1245425692236" ID="Freemind_Link_1348819723" MODIFIED="1245426070885" TEXT="DISPLACED activity and rule set"/>
+<node CREATED="1245426202363" ID="Freemind_Link_1235267206" MODIFIED="1245426211765" TEXT="DISPLACED coverage function defaults to 25/1000"/>
+<node CREATED="1245426052907" ID="Freemind_Link_1873524914" MODIFIED="1245426056981" TEXT="CAMP activity">
+<node CREATED="1245426083723" ID="Freemind_Link_1828876786" MODIFIED="1245426102661" TEXT="No rule set"/>
+<node CREATED="1245426103116" ID="Freemind_Link_1047983029" MODIFIED="1245426113301" TEXT="Presumption is that camps are away from locals in this spiral"/>
+</node>
+</node>
+</node>
+<node CREATED="1245426500507" ID="Freemind_Link_1107689017" MODIFIED="1245426504518" POSITION="right" TEXT="Demographics Model">
+<node CREATED="1245426504859" ID="Freemind_Link_802952236" MODIFIED="1245426515285" TEXT="Is good as presented (no new TBDs in document)"/>
+</node>
+<node CREATED="1245426327819" ID="Freemind_Link_133698814" MODIFIED="1245426334902" POSITION="right" TEXT="Attrition Model">
+<node CREATED="1245430579398" ID="Freemind_Link_330611963" MODIFIED="1245430584481" TEXT="NF Attacking ROEs">
+<node CREATED="1245430584966" ID="Freemind_Link_1850552399" MODIFIED="1245430598833" TEXT="NFs have three attacking ROEs">
+<node CREATED="1245430599142" ID="Freemind_Link_1419182332" MODIFIED="1245430603952" TEXT="HIT_AND_RUN">
+<node CREATED="1245430636631" ID="Freemind_Link_595426845" MODIFIED="1245430737886" TEXT="Same as existing &quot;ATTACK&quot; algorithm"/>
+</node>
+<node CREATED="1245430604486" ID="Freemind_Link_672343968" MODIFIED="1245430607408" TEXT="STAND_AND_FIGHT">
+<node CREATED="1245430649062" ID="Freemind_Link_1156628615" MODIFIED="1245430659648" TEXT="Turns the HIT_AND_RUN equation around"/>
+<node CREATED="1245430660278" ID="Freemind_Link_1361746821" MODIFIED="1245430731328" TEXT="Kill as many UF as we can regardless of what it costs"/>
+</node>
+<node CREATED="1245430607781" ID="Freemind_Link_1123863394" MODIFIED="1245430612640" TEXT="DO_NOT_ATTACK"/>
+</node>
+</node>
+<node CREATED="1245433985618" ID="Freemind_Link_1863583233" MODIFIED="1245433990285" TEXT="UF Defending ROEs">
+<node CREATED="1245433991233" ID="Freemind_Link_1584005254" MODIFIED="1245433998189" TEXT="UFs have three defending ROEs">
+<node CREATED="1245433998609" ID="Freemind_Link_1985731581" MODIFIED="1245434004764" TEXT="FIRE_BACK_IMMEDIATELY"/>
+<node CREATED="1245434005617" ID="Freemind_Link_887990736" MODIFIED="1245434009340" TEXT="FIRE_BACK_IF_PRESSED"/>
+<node CREATED="1245434009874" ID="Freemind_Link_361674041" MODIFIED="1245434012428" TEXT="HOLD_FIRE"/>
+</node>
+</node>
+<node CREATED="1245430356246" ID="Freemind_Link_1206436271" MODIFIED="1245430364385" TEXT="NF attack rate">
+<node CREATED="1245430364774" ID="Freemind_Link_724854627" MODIFIED="1245430402078" TEXT="Since the Economics model has been delayed, economic clout &#xa;is not available as a basis for the nominal attack rate."/>
+<node CREATED="1245430409334" ID="Freemind_Link_1482584143" MODIFIED="1245430423760" TEXT="Instead, the NF&apos;s attacking ROE will include a nominal rate of attacks per day."/>
+</node>
+<node CREATED="1245432622707" ID="Freemind_Link_508728028" MODIFIED="1245432631599" TEXT="Attrition to Civilian Units">
+<node CREATED="1245432632019" ID="Freemind_Link_1375064477" MODIFIED="1245432640419" TEXT="Is deferred to Spiral 2"/>
+</node>
+<node CREATED="1245426335275" ID="Freemind_Link_375661222" MODIFIED="1245426339718" TEXT="Kinds of units">
+<node CREATED="1245426340123" ID="Freemind_Link_20081166" MODIFIED="1245426347173" TEXT="We have UF and NF"/>
+<node CREATED="1245426347675" ID="Freemind_Link_850939547" MODIFIED="1245426392293" TEXT="Gary Phillips paper">
+<node CREATED="1245426355130" ID="Freemind_Link_847829820" MODIFIED="1245426363957" TEXT="Regular-conventional"/>
+<node CREATED="1245426364315" ID="Freemind_Link_1979278525" MODIFIED="1245426372165" TEXT="Regular-unconventional"/>
+<node CREATED="1245426372539" ID="Freemind_Link_1945118252" MODIFIED="1245426377109" TEXT="Irregular-conventional"/>
+<node CREATED="1245426377402" ID="Freemind_Link_1910989216" MODIFIED="1245426384438" TEXT="Irregular-unconventional"/>
+</node>
+<node CREATED="1245426394955" ID="Freemind_Link_641013376" MODIFIED="1245426406261" TEXT="Ultimately, we will probably want to use this terminology"/>
+</node>
+</node>
+</node>
+</map>
