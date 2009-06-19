8eaf8f4f1a4b6a826b42fcda1b4878f13c36378b
Will Duquette <William.H.Duquette@jpl.nasa.gov>
2009-06-19 20:08:04 +0000

    Updates from SME meeting

--- docs/dev/notes/SMEmeetingDecisions0906.mm
+++ docs/dev/notes/SMEmeetingDecisions0906.mm
@@ -10,9 +10,14 @@
 <node CREATED="1245425090796" ID="Freemind_Link_193760284" MODIFIED="1245425421846" TEXT="It&apos;s not clear whether doing I/S or C&amp;S should diminish the impact of the troop&apos;s main activity. "/>
 <node CREATED="1245425422556" ID="Freemind_Link_1566602261" MODIFIED="1245425448742" TEXT="Situation or series of event?"/>
 </node>
-<node CREATED="1245425469228" ID="Freemind_Link_917075298" MODIFIED="1245425471814" POSITION="right" TEXT="Shifts">
+<node CREATED="1245425469228" ID="Freemind_Link_917075298" MODIFIED="1245441279395" POSITION="right" TEXT="Activity Situations">
+<node CREATED="1245441284057" ID="Freemind_Link_1545096706" MODIFIED="1245441285667" TEXT="Shifts">
 <node CREATED="1245425472252" ID="Freemind_Link_170615432" MODIFIED="1245425491302" TEXT="The assigned-to-active ratio is simply the number of shifts per day."/>
 <node CREATED="1245425491852" ID="Freemind_Link_304605190" MODIFIED="1245425494198" TEXT="So call it that."/>
+<node CREATED="1245441374937" ID="Freemind_Link_1467877767" MODIFIED="1245441386563" TEXT="Need the number of shifts for each activity!">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
 </node>
 <node CREATED="1245425507165" ID="Freemind_Link_1211960668" MODIFIED="1245425576262" POSITION="right" TEXT="Displaced Persons/Refugees">
 <node CREATED="1245425513869" ID="Freemind_Link_736067438" MODIFIED="1245425550022" TEXT="Terminology">
@@ -78,6 +83,140 @@
 <node CREATED="1245426377402" ID="Freemind_Link_1910989216" MODIFIED="1245426384438" TEXT="Irregular-unconventional"/>
 </node>
 <node CREATED="1245426394955" ID="Freemind_Link_641013376" MODIFIED="1245426406261" TEXT="Ultimately, we will probably want to use this terminology"/>
+</node>
+</node>
+<node CREATED="1245436039103" ID="Freemind_Link_1319976621" MODIFIED="1245436044793" POSITION="right" TEXT="Ground Model: Ensits">
+<node CREATED="1245435695920" ID="Freemind_Link_696217278" MODIFIED="1245435705979" TEXT="Auto-Resolution">
+<node CREATED="1245435706368" ID="Freemind_Link_549606154" MODIFIED="1245435731546" TEXT="It&apos;s unreasonable to expect the analyst to manually resolve every ensit"/>
+<node CREATED="1245435738111" ID="Freemind_Link_766411256" MODIFIED="1245435759417" TEXT="The analyst should be able to specify a duration when creating the ensit."/>
+<node CREATED="1245435759968" ID="Freemind_Link_1026811239" MODIFIED="1245435787145" TEXT="The ensit will be resolved automatically after that time."/>
+<node CREATED="1245435788047" ID="Freemind_Link_904574603" MODIFIED="1245435799801" TEXT="The duration will have a ensit-type-specific default"/>
+<node CREATED="1245439751259" ID="Freemind_Link_941901754" MODIFIED="1245439762309" TEXT="Analyst must also specify who will resolve the ensit"/>
+</node>
+<node CREATED="1245435808864" ID="Freemind_Link_1958760423" MODIFIED="1245435811899" TEXT="Spawned ensits">
+<node CREATED="1245435845072" ID="Freemind_Link_400980395" MODIFIED="1245435862953" TEXT="Spawned ensits will be resolved with the spawning ensit">
+<node CREATED="1245439769115" ID="Freemind_Link_1828904445" MODIFIED="1245439775365" TEXT="And by the same resolver"/>
+</node>
+<node CREATED="1245435869855" ID="Freemind_Link_671899724" MODIFIED="1245435881147" TEXT="Some ensits might be spawned by multiple ensits">
+<node CREATED="1245435881456" ID="Freemind_Link_1508112599" MODIFIED="1245435894729" TEXT="E.g. BADFOOD and SEWAGE both spawn DISEASE"/>
+</node>
+<node CREATED="1245435812271" ID="Freemind_Link_598020469" MODIFIED="1245435919223" TEXT="Must track the trigger for each ensit, and resolve&#xa;when all triggering ensits are resolved."/>
+</node>
+</node>
+<node CREATED="1245436478367" ID="Freemind_Link_1339443761" MODIFIED="1245436480027" POSITION="right" TEXT="GRAM">
+<node CREATED="1245436480367" ID="Freemind_Link_1640125150" MODIFIED="1245436492107" TEXT="Slope effect bounds">
+<node CREATED="1245436492415" ID="Freemind_Link_206209073" MODIFIED="1245436502521" TEXT="At present, slope effects are bounded at +100 and -100"/>
+<node CREATED="1245436502799" ID="Freemind_Link_325448420" MODIFIED="1245436512025" TEXT="We need to allow slope effects to be bounded at smaller values"/>
+<node CREATED="1245436512447" ID="Freemind_Link_1489625471" MODIFIED="1245436572941" TEXT="Thus, the long term effect of SEWAGE can be limited to making &#xa;the civilians (say) mildly dissatisfied."/>
+<node CREATED="1245436574878" ID="Freemind_Link_752938003" MODIFIED="1245436588201" TEXT="There are some technical issues involving causes and scaling to be worked through"/>
+<node CREATED="1245436590319" ID="Freemind_Link_105923357" MODIFIED="1245436596489" TEXT="Deferred to Spiral 2"/>
+</node>
+<node CREATED="1245439677739" ID="Freemind_Link_972828845" MODIFIED="1245439680328" TEXT="Epsilons">
+<node CREATED="1245439680971" ID="Freemind_Link_1974965482" MODIFIED="1245439693269" TEXT="The GRAM &quot;epsilon&quot; of 0.1 is probably too large for Athena."/>
+</node>
+</node>
+<node CREATED="1245435127233" ID="Freemind_Link_536422318" MODIFIED="1245435132956" POSITION="right" TEXT="Driver Assessment Model">
+<node CREATED="1245435133681" ID="Freemind_Link_1170888984" MODIFIED="1245435162506" TEXT="&quot;ADA&quot; causes confusion, and Ben likes &quot;DAM&quot;, so we&apos;ll go with that."/>
+<node CREATED="1245435643519" ID="Freemind_Link_1772138416" MODIFIED="1245435661226" TEXT="Rob Crowson will scrub the rule magnitudes over time."/>
+<node CREATED="1245440902298" ID="Freemind_Link_1827735408" MODIFIED="1245440953992" TEXT="Spiral 2: When damage to facilities can spawn ensits, we&#xa;should indicate on each ensit&apos;s rule set page which facilities&#xa;can spawn the ensit."/>
+<node CREATED="1245435677456" ID="Freemind_Link_1723757900" MODIFIED="1245435679979" TEXT="BADFOOD">
+<node CREATED="1245435680288" ID="Freemind_Link_351582809" MODIFIED="1245435688410" TEXT="Spawns DISEASE after 1 day"/>
+<node CREATED="1245436026271" ID="Freemind_Link_1429405319" MODIFIED="1245436031336" TEXT="Auto-resolve after 10 days"/>
+</node>
+<node CREATED="1245436105535" ID="Freemind_Link_1578218713" MODIFIED="1245436110043" TEXT="BADWATER">
+<node CREATED="1245436112159" ID="Freemind_Link_1509965137" MODIFIED="1245436116281" TEXT="Spawns DISEASE after 1 day"/>
+<node CREATED="1245436116559" ID="Freemind_Link_318409517" MODIFIED="1245436120057" TEXT="Auto-resolve after 5 days"/>
+<node CREATED="1245436120399" ID="Freemind_Link_779066530" MODIFIED="1245436144539" TEXT="Spiral 2">
+<node CREATED="1245436144975" ID="Freemind_Link_1591795456" MODIFIED="1245436153497" TEXT="Does boiling remove the contamination?"/>
+<node CREATED="1245436159103" ID="Freemind_Link_1652458519" MODIFIED="1245436186137" TEXT="Add a second rule set, or add a parameter to instances of this rule set"/>
+</node>
+</node>
+<node CREATED="1245436211920" ID="Freemind_Link_590971882" MODIFIED="1245436213883" TEXT="BIO">
+<node CREATED="1245436214351" ID="Freemind_Link_1566231119" MODIFIED="1245436239929" TEXT="Once WMDs are used, Stability Operations are over"/>
+<node CREATED="1245436243552" ID="Freemind_Link_1442825141" MODIFIED="1245436247513" TEXT="Delete from Athena"/>
+</node>
+<node CREATED="1245436249663" ID="Freemind_Link_1096610044" MODIFIED="1245436250953" TEXT="CHEM">
+<node CREATED="1245436214351" ID="Freemind_Link_194688728" MODIFIED="1245436239929" TEXT="Once WMDs are used, Stability Operations are over"/>
+<node CREATED="1245436243552" ID="Freemind_Link_1943228046" MODIFIED="1245436247513" TEXT="Delete from Athena"/>
+</node>
+<node CREATED="1245436284911" ID="Freemind_Link_448047501" MODIFIED="1245436287163" TEXT="COMMOUT">
+<node CREATED="1245436289711" ID="Freemind_Link_1891512638" MODIFIED="1245436331544" TEXT="Auto-resolve after 7 days"/>
+</node>
+<node CREATED="1245436319871" ID="Freemind_Link_868941021" MODIFIED="1245436321531" TEXT="DISASTER">
+<node CREATED="1245436321983" ID="Freemind_Link_212691431" MODIFIED="1245436327536" TEXT="Auto-resolve after 45 days"/>
+</node>
+<node CREATED="1245436387376" ID="Freemind_Link_1774910412" MODIFIED="1245436389435" TEXT="DISEASE">
+<node CREATED="1245436321983" ID="Freemind_Link_1001301447" MODIFIED="1245436399712" TEXT="Auto-resolve after 30 days"/>
+</node>
+<node CREATED="1245436422143" ID="Freemind_Link_1356127433" MODIFIED="1245436424511" TEXT="EPIDEMIC">
+<node CREATED="1245436425486" ID="Freemind_Link_1311276520" MODIFIED="1245436432680" TEXT="Auto-resolve after 360 days"/>
+<node CREATED="1245436446030" ID="Freemind_Link_1045249270" MODIFIED="1245436470169" TEXT="Consider having the slope decay over time"/>
+</node>
+<node CREATED="1245439639771" ID="Freemind_Link_135793531" MODIFIED="1245439644744" TEXT="FOODSHRT">
+<node CREATED="1245439657387" ID="Freemind_Link_192374470" MODIFIED="1245439662773" TEXT="Auto-resolve after 180 days"/>
+</node>
+<node CREATED="1245439723915" ID="Freemind_Link_1530261012" MODIFIED="1245439726168" TEXT="FUELSHRT">
+<node CREATED="1245439726922" ID="Freemind_Link_216144712" MODIFIED="1245439731061" TEXT="Auto-resolve after 30 days"/>
+</node>
+<node CREATED="1245439826458" ID="Freemind_Link_1389886921" MODIFIED="1245439828328" TEXT="GARBAGE">
+<node CREATED="1245439828922" ID="Freemind_Link_1113752693" MODIFIED="1245439835284" TEXT="Auto-resolve after 45 days"/>
+</node>
+<node CREATED="1245439847276" ID="Freemind_Link_1833383098" MODIFIED="1245439849704" TEXT="INDSPILL">
+<node CREATED="1245439850171" ID="Freemind_Link_1729759464" MODIFIED="1245439858501" TEXT="Spawns DISEASE after 5 days"/>
+<node CREATED="1245439859067" ID="Freemind_Link_511079658" MODIFIED="1245439862964" TEXT="Auto-resolve after 90 days"/>
+</node>
+<node CREATED="1245440624298" ID="Freemind_Link_245535087" MODIFIED="1245440627432" TEXT="MINEFIELD">
+<node CREATED="1245440627881" ID="Freemind_Link_1012584880" MODIFIED="1245440635924" TEXT="New situation: presence of minefields in neighborhood"/>
+<node CREATED="1245440636297" ID="Freemind_Link_1267365315" MODIFIED="1245440643812" TEXT="Copy from ORDNANCE"/>
+<node CREATED="1245440644266" ID="Freemind_Link_423274861" MODIFIED="1245440679572" TEXT="Auto-resolve after 1080 days (three years)"/>
+<node CREATED="1245440800826" ID="Freemind_Link_1139538528" MODIFIED="1245440806012" TEXT="Same &quot;cause&quot; as ORDNANCE"/>
+<node CREATED="1245440701177" ID="Freemind_Link_1904443543" MODIFIED="1245440710148" TEXT="Slope can decline over time, but not to zero">
+<node CREATED="1245440712843" ID="Freemind_Link_696592691" MODIFIED="1245440720052" TEXT="Necessary if we implement slope bounds?"/>
+</node>
+</node>
+<node CREATED="1245439884763" ID="Freemind_Link_995322584" MODIFIED="1245439889419" TEXT="MOSQUE">
+<node CREATED="1245439890235" ID="Freemind_Link_1740344814" MODIFIED="1245439924149" TEXT="Replace with two ensits">
+<node CREATED="1245439926827" ID="Freemind_Link_1192829387" MODIFIED="1245439933176" TEXT="SACRED">
+<node CREATED="1245439933419" ID="Freemind_Link_451287552" MODIFIED="1245439946164" TEXT="Damage to sacred site/artifact"/>
+<node CREATED="1245439947515" ID="Freemind_Link_820819723" MODIFIED="1245439957108" TEXT="Same as MOSQUE is now"/>
+<node CREATED="1245440078763" ID="Freemind_Link_1153491352" MODIFIED="1245440083408" TEXT="Cause is &quot;SACRED&quot;"/>
+<node CREATED="1245439957434" ID="Freemind_Link_193410696" MODIFIED="1245439968868" TEXT="Auto-resolve after 45 days"/>
+</node>
+<node CREATED="1245439970746" ID="Freemind_Link_1457753943" MODIFIED="1245439975400" TEXT="CULTURAL">
+<node CREATED="1245439977899" ID="Freemind_Link_305078017" MODIFIED="1245439983716" TEXT="Damage to cultural site/artifact"/>
+<node CREATED="1245439984330" ID="Freemind_Link_94122344" MODIFIED="1245439992100" TEXT="Like MOSQUE, but 1 stop down"/>
+<node CREATED="1245440084043" ID="Freemind_Link_867924263" MODIFIED="1245440097253" TEXT="Cause is &quot;CULTURAL&quot;"/>
+<node CREATED="1245439992938" ID="Freemind_Link_1284312338" MODIFIED="1245439998932" TEXT="Auto-resolve after 45 days"/>
+<node CREATED="1245440342699" ID="Freemind_Link_773741912" MODIFIED="1245440348397" TEXT="Is there a better name?"/>
+</node>
+</node>
+<node CREATED="1245440057435" ID="Freemind_Link_1246742546" MODIFIED="1245440071112" TEXT="Should SACRED and CULTURAL have the same &quot;cause&quot;?">
+<icon BUILTIN="help"/>
+<node CREATED="1245440102555" ID="Freemind_Link_1965158558" MODIFIED="1245440105652" TEXT="And if so, what is it?"/>
+</node>
+</node>
+<node CREATED="1245440411339" ID="Freemind_Link_464359339" MODIFIED="1245440414040" TEXT="NOWATER">
+<node CREATED="1245440414650" ID="Freemind_Link_134056780" MODIFIED="1245440421972" TEXT="Now titled &quot;Interrupted Water Supply&quot;"/>
+<node CREATED="1245440422986" ID="Freemind_Link_1262591065" MODIFIED="1245440433428" TEXT="Spawns DISEASE after 1 day"/>
+<node CREATED="1245440433754" ID="Freemind_Link_1900695308" MODIFIED="1245440443668" TEXT="Auto-resolve after 3 days"/>
+</node>
+<node CREATED="1245440511786" ID="Freemind_Link_20811368" MODIFIED="1245440514504" TEXT="ORDNANCE">
+<node CREATED="1245440515194" ID="Freemind_Link_1488323304" MODIFIED="1245440537332" TEXT="Note that this is usually from cluster munitions; if there&apos;s one, there are others"/>
+<node CREATED="1245440580330" ID="Freemind_Link_698357705" MODIFIED="1245440588420" TEXT="Auto-resolve after 540 days"/>
+</node>
+<node CREATED="1245440985482" ID="Freemind_Link_1226178890" MODIFIED="1245440991000" TEXT="PIPELINE">
+<node CREATED="1245440991321" ID="Freemind_Link_325013577" MODIFIED="1245440996332" TEXT="Auto-resolve after 7 days"/>
+</node>
+<node CREATED="1245441043322" ID="Freemind_Link_1620231322" MODIFIED="1245441045384" TEXT="POWEROUT">
+<node CREATED="1245441045753" ID="Freemind_Link_1721662185" MODIFIED="1245441051379" TEXT="Auto-resolve after 60 days"/>
+<node CREATED="1245441051785" ID="Freemind_Link_1741886830" MODIFIED="1245441102954" TEXT="Spiral 2:  To assess economic impacts, we&apos;ll need to distinguish &#xa;between total outages and intermitted outages"/>
+</node>
+<node CREATED="1245441143162" ID="Freemind_Link_1285323831" MODIFIED="1245441144691" TEXT="REFINERY">
+<node CREATED="1245441145593" ID="Freemind_Link_1229183235" MODIFIED="1245441166844" TEXT="Auto-resolve after 5 days"/>
+</node>
+<node CREATED="1245441180042" ID="Freemind_Link_533444573" MODIFIED="1245441181597" TEXT="SEWAGE">
+<node CREATED="1245441182393" ID="Freemind_Link_142193041" MODIFIED="1245441190900" TEXT="Spawns DISEASE after 30 days"/>
+<node CREATED="1245441885880" ID="Freemind_Link_457657659" MODIFIED="1245441910434" TEXT="Auto-resolve after 60 days"/>
 </node>
 </node>
 </node>
