f4311b03b0a7164c0268df51a563aa52d9b4ca50
Will Duquette <William.H.Duquette@jpl.nasa.gov>
2012-01-10 16:37:23 +0000

    Fixed Bug #{3292}
    The "entities" view now includes all agents, not just agents that are actors.

--- lib/app_sim/gui_views.sql
+++ lib/app_sim/gui_views.sql
@@ -683,8 +683,7 @@ WHERE nbhoods.local;
 -- this, and retrieve the entity type for a given ID.  The list
 -- includes a number of reserved words.
 --
--- TBD: Should we just have an "entities" table and insert a row
--- into it for each entity?
+-- Note: The agents table includes all actors.
 
 CREATE TEMPORARY VIEW entities AS
 SELECT 'PLAYBOX' AS id, 'reserved' AS etype                  UNION
@@ -695,7 +694,7 @@ SELECT 'ALL'     AS id, 'reserved' AS etype                  UNION
 SELECT 'NONE'    AS id, 'reserved' AS etype                  UNION
 SELECT 'SELF'    AS id, 'reserved' AS etype                  UNION
 SELECT n         AS id, 'nbhood'   AS etype FROM nbhoods     UNION
-SELECT a         AS id, 'actor'    AS etype FROM actors      UNION
+SELECT agent_id  AS id, 'agent'    AS etype FROM agents      UNION
 SELECT g         AS id, 'group'    AS etype FROM groups      UNION
 SELECT c         AS id, 'concern'  AS etype FROM concerns    UNION
 SELECT a         AS id, 'activity' AS etype FROM activity    UNION
