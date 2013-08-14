615ab08e5581d2e89d6e7a9ac26844a272d4d500
Will Duquette <William.H.Duquette@jpl.nasa.gov>
2013-08-14 20:21:02 +0000

    Fixed Bug #{3832}
    Added the SA Flag to the Demog/Groups browser.

--- lib/app_sim/ui/demogbrowser.tcl
+++ lib/app_sim/ui/demogbrowser.tcl
@@ -32,12 +32,15 @@ snit::widgetadaptor demogbrowser {
     # %D is replaced with the color for derived columns.
 
     typevariable layout {
-        { g           "\nGroup"                                              }
-        { n           "\nNbhood"                                             }
-        { basepop     "Base\nPopulation"    -sortmode integer                }
-        { pop_cr      "Change Rate\n%/year"                                  }
-        { population  "Current\nPopulation" -sortmode integer -foreground %D }
-        { attrition   "\nAttrition"         -sortmode integer -foreground %D }
+        { g              "\nGroup"                                }
+        { n              "\nNbhood"                               }
+        { basepop        "Base\nPopulation"    -sortmode integer  }
+        { pretty_sa_flag "Subsist.\nAgric."                       }
+        { pop_cr         "Change Rate\n%/year"                    }
+        { population     "Current\nPopulation" 
+                         -sortmode integer -foreground %D         }
+        { attrition      "\nAttrition"         
+                         -sortmode integer -foreground %D         }
     }
 
     #--------------------------------------------------------------------
