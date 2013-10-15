852eafaf8ec03a02f88f88a9f743daff9960e90d
Dave Hanks <David.R.Hanks@jpl.nasa.gov>
2013-10-15 21:46:19 +0000

    Fixed Bug #{4165}
    Done!
    
    Suggested tests:
    
    * Create a block and add WITHDRAW tactics with different modes to it
    * Verify that the tactic executes as described or fails if conditions warrant
    * Verify that the help pages are accurate
    
    The automated test suite is updated.

--- lib/app_sim/shared/tactic_withdraw.tcl
+++ lib/app_sim/shared/tactic_withdraw.tcl
@@ -17,7 +17,12 @@ tactic define WITHDRAW "Withdraw Money" {actor} {
     #-------------------------------------------------------------------
     # Instance Variables
 
+    variable mode    ;# ALL, EXACT, UPTO, PERCENT or BORROW
     variable amount  ;# Amount of money to withdraw
+    variable percent ;# Percent of money to withdraw if mode is PERCENT
+
+    # Transient data
+    variable trans
 
     #-------------------------------------------------------------------
     # Constructor
@@ -27,7 +32,11 @@ tactic define WITHDRAW "Withdraw Money" {actor} {
         next
 
         # Initialize state variables
-        set amount 0.0
+        set mode    ALL
+        set amount  0.0
+        set percent 0.0
+
+        set trans(amount) 0.0
 
         # Save the options
         my configure {*}$args
@@ -39,7 +48,33 @@ tactic define WITHDRAW "Withdraw Money" {actor} {
     # No special SanityCheck is required.
 
     method narrative {} {
-        return "Withdraw \$[moneyfmt $amount] from the cash reserve."
+        set amt [moneyfmt $amount]
+
+        switch -exact -- $mode {
+            ALL {
+                return "Withdraw all money from cash reserve."
+            }
+
+            EXACT {
+                return "Withdraw \$$amt from cash reserve."
+            }
+
+            UPTO {
+                return "Withdraw up to \$$amt from cash reserve."
+            }
+
+            PERCENT {
+                return "Withdraw $percent% of cash reserve."
+            }
+
+            BORROW {
+                return "Withdraw \$$amt from cash reserve, borrowing if necessary."
+            }
+
+            default {
+                error "Invalid mode: \"$mode\""
+            }
+        }
     }
 
     # obligate coffer
@@ -55,16 +90,60 @@ tactic define WITHDRAW "Withdraw Money" {actor} {
     method obligate {coffer} {
         assert {[strategy ontick]}
 
-        $coffer withdraw $amount
+        # FIRST, retrieve the current reserve amount
+        let cash_reserve [$coffer reserve]
+        set withdrawal 0.0
+
+        # NEXT, depending on mode, try to withdraw money
+        switch -exact -- $mode {
+            ALL {
+                if {$cash_reserve > 0.0} {
+                    set withdrawal $cash_reserve
+                }
+            }
+
+            EXACT {
+                # This is the only one that can fail
+                if {$amount > $cash_reserve} {
+                    return 0
+                }
+
+                set withdrawal $amount
+            }
+
+            UPTO {
+                let withdrawal {max(0.0, min($cash_reserve, $amount))}
+            }
+
+            PERCENT {
+                if {$cash_reserve > 0.0} {
+                    let withdrawal {double($percent/100.0) * $cash_reserve}
+                }
+            }
+
+            BORROW {
+                set withdrawal $amount
+            }
+
+            default {
+                error "Invalid mode: \"$mode\""
+            }
+        }
+
+        # NEXT, get the actual amount of the withdrawal
+        set trans(amount) $withdrawal
+
+        # NEXT, obligate it
+        $coffer withdraw $trans(amount)
 
         return 1
     }
 
     method execute {} {
-        cash withdraw [my agent] $amount
+        cash withdraw [my agent] $trans(amount)
 
         sigevent log 2 tactic "
-            WITHDRAW: [my agent] withdraws \$[moneyfmt $amount] from reserve.
+            WITHDRAW: [my agent] withdraws \$[moneyfmt $trans(amount)] from reserve.
         " [my agent]
     }
 }
@@ -85,21 +164,64 @@ order define TACTIC:WITHDRAW {
         text tactic_id -context yes \
             -loadcmd {beanload}
 
-        rcc "Amount:" -for amount
-        text amount
+        rcc "Mode:"   -for mode
+        selector mode {
+            case ALL "Withdraw all available money from cash reserve" {}
+
+            case EXACT "Withdraw exactly this much from cash reserve" {
+                rcc "Amount:" -for amount
+                text amount
+            }
+
+            case UPTO "Withdraw up to this much from cash reserve" {
+                rcc "Amount:" -for amount
+                text amount
+            }
+
+            case PERCENT "Withdraw this percentage of cash reserve" {
+                rcc "Percent:" -for percent
+                text percent
+                label "%"
+            }
+
+            case BORROW "Withdraw from cash reserve, borrowing if necessary" {
+                rcc "Amount:" -for amount
+                text amount
+            }
+        }
     }
 } {
     # FIRST, prepare the parameters
     prepare tactic_id  -required           -oneof [tactic::WITHDRAW ids]
-    prepare amount     -required -toupper  -type money
 
-    returnOnError -final
+    returnOnError
 
     # NEXT, get the tactic
     set tactic [tactic get $parms(tactic_id)]
 
+    prepare mode    -toupper -selector
+    prepare amount  -toupper -type money
+    prepare percent -toupper -type rpercent
+
+    returnOnError 
+
+    # NEXT, do cross checks
+    fillparms parms [$tactic view]
+
+    if {$parms(mode) ne "PERCENT" &&
+        $parms(mode) ne "ALL"     &&
+        $parms(amount) == 0.0} {
+            reject amount "You must specify an amount > 0.0"
+    }
+
+    if {$parms(mode) eq "PERCENT" && $parms(percent) == 0.0} {
+        reject percent "You must specify a percent > 0.0"
+    }
+
+    returnOnError -final
+
     # NEXT, update the tactic, saving the undo script
-    set undo [$tactic update_ {amount} [array get parms]]
+    set undo [$tactic update_ {mode amount percent} [array get parms]]
 
     # NEXT, modify the tactic
     setundo $undo
