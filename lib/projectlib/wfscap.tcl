83a7d797137ec1323d49d674a267f0db7b152ac7
Dave Hanks <David.R.Hanks@jpl.nasa.gov>
2014-05-13 17:13:05 +0000

    Fixed Bug #{4499}
    Done.
    
    No I&T at this time.  If we get into the business of dealing with Web Feature Services (which is likely if the DCGS-A effort goes forward), then this will get tested.

--- lib/projectlib/wfscap.tcl
+++ lib/projectlib/wfscap.tcl
@@ -38,12 +38,6 @@ namespace eval ::projectlib:: {
 
 snit::type ::projectlib::wfscap {
     #-------------------------------------------------------------------
-    # Type Constructor
-    typeconstructor {
-        namespace import ::marsutil::*
-    }
-
-    #-------------------------------------------------------------------
     # Type Variables
 
     # wfsdata - the Web Feature Service data returned
@@ -51,6 +45,19 @@ snit::type ::projectlib::wfscap {
 
     # wfsversion - the WMS version this parser supports
     typevariable wfsversion "1.1.0"
+
+    #-------------------------------------------------------------------
+    # Type Components
+
+    typecomponent dp  ;# domparser(n) object
+
+    #-------------------------------------------------------------------
+    # Type Constructor
+
+    typeconstructor {
+        namespace import ::marsutil::*
+        set dp ::projectlib::domparser
+    }
 
     #-------------------------------------------------------------------
     # Type Methods
@@ -64,6 +71,7 @@ snit::type ::projectlib::wfscap {
     # traversed and the WFS dictionary filled in.
 
     typemethod parse {xml} {
+
         # FIRST, default values for the WFS dictionary of data
         dict set wfsdata Version "unknown"
         dict set wfsdata Operation ""
@@ -71,9 +79,9 @@ snit::type ::projectlib::wfscap {
         dict set wfsdata Constraints ""
 
         # NEXT, create the DOM and get the top node
-        set doc [dom parse $xml]
-        set root [$doc documentElement]
-        set topNode [$doc getElementsByTagName WFS_Capabilities]
+        set doc     [$dp doc $xml]
+        set root    [$dp root]
+        set topNode [$dp nodebyname WFS_Capabilities]
 
         # NEXT check to see if this is the right format and version
         if {$topNode eq ""} {
@@ -81,7 +89,7 @@ snit::type ::projectlib::wfscap {
                 "Not WFS Capabilities data."
         }
 
-        set vers [$type getAttribute $topNode "version"]
+        set vers [$dp attr $topNode "version"]
 
         if {$vers ne $wfsversion} {
             return -code error -errorcode VERSION \
@@ -94,8 +102,8 @@ snit::type ::projectlib::wfscap {
         # NEXT, parse constraints
         foreach node [$root selectNodes /*] {
             foreach cons [$node getElementsByTagName ows:Constraint] {
-                set name [$cons getAttribute name]
-                set val [$type getElementText $cons "ows:Value"]
+                set name [$dp attr $cons name]
+                set val [$dp ctextbyname $cons "ows:Value"]
                 dict lappend wfsdata Constraints $name $val
             }
         }
@@ -103,9 +111,9 @@ snit::type ::projectlib::wfscap {
         # NEXT, parse available WFS operations
         foreach node [$root selectNodes /*] {
              foreach op [$node getElementsByTagName ows:Operation] {
-                 set operation [$op getAttribute name]
-                 set urlNode [$type getChildNodeByName $op "ows:Get"]
-                 set url [$type getAttribute $urlNode xlink:href]
+                 set operation [$dp attr $op name]
+                 set urlNode [$dp cnodebyname $op "ows:Get"]
+                 set url [$dp attr $urlNode xlink:href]
 
                  set d [dict create $operation [dict create Xref $url]]
                  set newd [dict merge $d [dict get $wfsdata Operation]]
@@ -116,9 +124,9 @@ snit::type ::projectlib::wfscap {
         # NEXT, parse available WFS feature types
         foreach node [$root selectNodes /*] {
             foreach feature [$node getElementsByTagName FeatureType] {
-                set name [$type getElementText $feature Name]
-                set title [$type getElementText $feature Title]
-                set srs   [$type getElementText $feature DefaultSRS]
+                set name [$dp ctextbyname $feature Name]
+                set title [$dp ctextbyname $feature Title]
+                set srs   [$dp ctextbyname $feature DefaultSRS]
 
                 set d [dict create $name [dict create Title $title SRS $srs]]
                 set newd [dict merge $d [dict get $wfsdata FeatureType]]
@@ -127,7 +135,7 @@ snit::type ::projectlib::wfscap {
         }
 
         # NEXT, delete DOM and return data
-        $doc delete
+        $dp delete
 
         return $wfsdata
     }
@@ -147,104 +155,6 @@ snit::type ::projectlib::wfscap {
         close $f
 
         $type parse $xml
-    }
-
-    # getChildNodeByName node tag
-    #
-    # node   - a DOM node
-    # tag    - a tag to search for in the subtree below node
-    #
-    # This method expects to find one and only one child with the 
-    # supplied tag as a name somewhere below node in the DOM if 
-    # found, the node is returned otherwise the empty string is
-    # returned.  Error if there is more than one child with the
-    # supplied tag.
-
-    typemethod getChildNodeByName {node tag} {
-        set children [$node getElementsByTagName $tag]
-
-        if {[llength $children] == 0} {
-            return ""
-        } elseif {[llength $children] > 1} {
-            error "Too many $tag elements in [$node nodeName]"
-        } 
-
-        set child [lindex $children 0]
-        return $child
-    }
-
-    # getElementText  node tag
-    #
-    # node   - a DOM node
-    # tag    - a tag to search for in the subtree below node
-    #
-    # This method expects to find one and only one child with the
-    # supplied tag as a name somewhere below node in the DOM. If
-    # found, the text enclosed by the start and end tag is returned
-    # otherwise the empty string is returned.  Error if there is 
-    # more than one child with the supplied tag.
-
-    typemethod getElementText {node tag} {
-        set children [$node getElementsByTagName $tag]
-
-        if {[llength $children] == 0} {
-            return ""
-        } elseif {[llength $children] > 1} {
-            error "Too many $tag elements in [$node nodeName]"
-        } else {
-            set child [lindex $children 0]
-            
-            return [$child text]
-        }
-    }
-
-    # getElementsAsList node tag ?caps?
-    #
-    # node  - a DOM node
-    # tag   - a tag to search for in the subtree below node
-    # caps  - if 1, return all caps otherwise return text as found
-    #         default is 0
-    #
-    # This method searches the subtree below node for all children
-    # with the supplied tag. If found, a list of the text enclosed
-    # by the start and end tag is returned otherwise an empty string
-    # is returned.
-
-    typemethod getElementsAsList {node tag {caps 0}} {
-        set children [$node getElementsByTagName $tag]
-
-        if {[llength $children] == 0} {
-            return ""
-        } else {
-            set clist [list]
-            
-            foreach child $children {
-                if {$caps} {
-                    lappend clist [string toupper [$child text]]
-                } else {
-                    lappend clist [$child text]
-                }
-            }
-
-            return $clist
-        }
-    }
-
-
-    # getAttribute node name
-    #
-    # node   - a DOM node
-    # name   - the name of an attribute in node
-    #
-    # This method returns the value of an attribute found within
-    # the supplied node. If not found, an error is generated.
-
-    typemethod getAttribute {node name} {
-        if {[$node hasAttribute $name]} {
-            return [$node getAttribute $name]
-        } else {
-            error "[$node nodeName]: no attribute named $name"
-        }
     }
 }
 
