354c23932832f0398646bac2a79c41450f737278
Dave Hanks <David.R.Hanks@jpl.nasa.gov>
2014-05-23 18:59:25 +0000

    Fixed Bug #{4513}
    Fixed.
    
    Turns out that it doesn't make any sense to try to compute ALOC, ELOC and Poverty % from a user supplied expectf. It's not possible to come up with numbers that make sense.
    
    Ultimately, this tab will become invisible to the user when the economic model is disabled.
    
    For now, the browser has two views: one for when the economic model is enabled, and one for when it is disabled.
    
    When disabled, the columns that do not make sense are invisible to the user.
    
    Retest.

--- lib/app_sim/ui/econngbrowser.tcl
+++ lib/app_sim/ui/econngbrowser.tcl
@@ -31,20 +31,51 @@ snit::widgetadaptor econngbrowser {
     #
     # %D is replaced with the color for derived columns.
 
-    typevariable layout {
-        { g           "Civ\nGroup"                                       }
-        { longname    "Long\nName"                                      }
-        { n           "\nNbhood"                                         }
-        { population  "\nPop."           -sortmode integer -foreground %D }
-        { subsistence "\nSubsist."       -sortmode integer -foreground %D }
-        { consumers   "\nConsumers"      -sortmode integer -foreground %D }
-        { labor_force "Labor\nForce"     -sortmode integer -foreground %D }
-        { unemployed  "\nUnemployed"     -sortmode integer -foreground %D }
-        { ur          "Unemp.\nRate%"    -sortmode real    -foreground %D }
-        { aloc        "Actual\nLOC"      -sortmode real    -foreground %D }
-        { eloc        "Expected\nLOC"    -sortmode real    -foreground %D }
-        { rloc        "Required\nLOC"    -sortmode real    -foreground %D }
-        { povpct      "\nPoverty%"       -sortmode real    -foreground %D }
+    typevariable modes -array {
+        econ_enabled {
+            -layout {
+                { g           "Civ\nGroup"                                     }
+                { longname    "Long\nName"                                     }
+                { n           "\nNbhood"                                       }
+                { population  "\nPop."        -sortmode integer -foreground %D }
+                { subsistence "\nSubsist."    -sortmode integer -foreground %D }
+                { consumers   "\nConsumers"   -sortmode integer -foreground %D }
+                { labor_force "Labor\nForce"  -sortmode integer -foreground %D }
+                { unemployed  "\nUnemployed"  -sortmode integer -foreground %D }
+                { ur          "Unemp.\nRate%" -sortmode real    -foreground %D }
+                { aloc        "Actual\nLOC"   -sortmode real    -foreground %D }
+                { eloc        "Expected\nLOC" -sortmode real    -foreground %D }
+                { rloc        "Required\nLOC" -sortmode real    -foreground %D }
+                { povpct      "\nPoverty%"    -sortmode real    -foreground %D }
+            }
+            -reloadon {
+                ::sim <DbSyncB>
+                ::demog <Update>
+                ::rdb <nbhoods>
+                ::rdb <groups>
+                ::rdb <civgroups>
+            }
+        }
+        econ_disabled {
+            -layout {
+                { g           "Civ\nGroup"                                     }
+                { longname    "Long\nName"                                     }
+                { n           "\nNbhood"                                       }
+                { population  "\nPop."        -sortmode integer -foreground %D }
+                { subsistence "\nSubsist."    -sortmode integer -foreground %D }
+                { consumers   "\nConsumers"   -sortmode integer -foreground %D }
+                { labor_force "Labor\nForce"  -sortmode integer -foreground %D }
+                { unemployed  "\nUnemployed"  -sortmode integer -foreground %D }
+                { ur          "Unemp.\nRate%" -sortmode real    -foreground %D }
+            }
+            -reloadon {
+                ::sim <DbSyncB>
+                ::demog <Update>
+                ::rdb <nbhoods>
+                ::rdb <groups>
+                ::rdb <civgroups>
+            }
+        }
     }
 
     #--------------------------------------------------------------------
@@ -57,13 +88,10 @@ snit::widgetadaptor econngbrowser {
             -view         gui_econ_g                  \
             -uid          id                          \
             -titlecolumns 1                           \
-            -reloadon {
-                ::sim <DbSyncB>
-                ::demog <Update>
-                ::rdb <nbhoods>
-                ::rdb <groups>
-                ::rdb <civgroups>
-            } -layout [string map [list %D $::app::derivedfg] $layout]
+            {*}[ModeOptions econ_disabled]
+
+        notifier bind ::econ <State> $self [mymethod EconChange]
+        notifier bind ::sim  <State> $self [mymethod SimChange]
 
         # NEXT, get the options.
         $self configurelist $args
@@ -73,6 +101,58 @@ snit::widgetadaptor econngbrowser {
     # Public Methods
 
     delegate method * to hull
+
+    # EconChange
+    #
+    # Responds to a change in the econ model while the simulation is
+    # running.  It's possible that the econ model will disable itself
+    # if it runs into problems.
+
+    method EconChange {} {
+        if {[econ state] eq "DISABLED"} {
+            $self SetMode econ_disabled
+        } else {
+            $self SetMode econ_enabled
+        }
+    }
+
+    # SimChange
+    #
+    # This method makes sure the correct layout is used when the scenario
+    # is locked.
+
+    method SimChange {} {
+        if {[econ state] eq "DISABLED"} {
+            $self SetMode econ_disabled
+        } else {
+            $self SetMode econ_enabled
+        }
+    }
+
+    # SetMode mode
+    #
+    # mode - econ_disabled | econ_enabled
+    #
+    # Sets the widget to display the content for the given mode.
+
+    method SetMode {mode} {
+        $hull configure {*}[ModeOptions $mode]
+    }
+
+    # ModeOptions mode
+    #
+    # mode - econ_disabled | econ_enabled
+    #
+    # Returns a dictionary of the mode options and values.
+
+    proc ModeOptions {mode} {
+        set opts $modes($mode)
+        dict with opts {
+            set -layout [string map [list %D $::app::derivedfg] ${-layout}]
+        }
+
+        return $opts
+    }
 }
 
 
